fe28dfba7dc97369f43db76ae645d5d210ecb7fd
Hassan <hr8581492gmail.com>
2024-10-11 11:17:38 -0700

    ok

---
+++ Quarter 1/for_loops.py
@@ -0,0 +1,7 @@
+for i in range(1, 4):
+    print(i)
+    for j in range(5, 9):
+        print(i, j)#nested if
+
+for x in range(8):
+    print(x)
---
+++ highlow.py
@@ -0,0 +1,18 @@
+# import random
+
+
+
+# player_number = random.randint(1 , 100)
+# print(player_number)
+# computer_number = random.randint(1 , 100)
+
+# print(f"players number:{player_number}")
+# print(f"computer number:{computer_number}")
+
+
+# print(f"your number is:{player_number}")
+
+# guess = input("do you think your number is higher or lower than the computer's?")
+# print(f"your guessed:{guess}")
+
+# welcome()
