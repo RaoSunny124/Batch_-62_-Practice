66b617fa17d0a8a9805a9d811d388ef899fd0b55
Hassan <hr8581492gmail.com>
2024-10-13 10:02:49 -0700

    now ok

--- highlow.py
+++ highlow.py
@@ -26,20 +26,3 @@ while count < 3:
 
     print(f'your score is now: {score}')
     print(f'\nGame over! your final score is:{score}')    
-
-
-
-# player_number = random.randint(1 , 100)
-# print(player_number)
-# computer_number = random.randint(1 , 100)
-
-# print(f"players number:{player_number}")
-# print(f"computer number:{computer_number}")
-
-
-# print(f"your number is:{player_number}")
-
-# guess = input("do you think your number is higher or lower than the computer's?")
-# print(f"your guessed:{guess}")
-
-# welcome()
