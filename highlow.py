165b4838ca30f1bb1696b7832b6a36ff56b4c7ad
Hassan <hr8581492gmail.com>
2024-10-13 09:57:10 -0700

    now done

--- highlow.py
+++ highlow.py
@@ -1,32 +1,31 @@
 import random
 score = 0
 count = 0
-while count < 3:score = 0
+while count < 3:
     computer_number = random.randint(1 , 100)
-    print('computer_number is' , computer_number)
     player_number = random.randint(1 , 100)
     print('player number is: ' , player_number)
     user_guess = input("Do u think your number is higher or lower than the computer's? higher\lower:")
 
 
     if user_guess == 'higher':
-        if player_number > computer_number :
-            print('u are correct' , computer_number)
-            score += 1
-        else:
-            print('u are wrong' , computer_number)
+            if player_number > computer_number :
+                print('u are correct' , computer_number)
+                score += 1
+            else:
+                print('u are wrong' , computer_number)
     elif user_guess == 'lower':
-        if  player_number < computer_number :
-            print('u are the correct' , computer_number)
-            score += 1
-        else:
-            print('U are wrong' , computer_number)
-            
+            if  player_number < computer_number :
+                print('u are the correct' , computer_number)
+                score += 1
+            else:
+                print('U are wrong' , computer_number)
+                
     else:
-        print('Invalid synthax : Plz try higher or lower')            
+            print('Invalid synthax : Plz try higher or lower')            
 
     print(f'your score is now: {score}')
-print(f'\nGame over! your final score is:{score}')    
+    print(f'\nGame over! your final score is:{score}')    
 
 
 
